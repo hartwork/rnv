c7e07fec649e690388cf7def05128ad090665be2
David Tolpin <dvd@davidashen.net>
2003-12-15 22:25:12 +0000

    attribute is reported with value
    svn path=/trunk/trunk/; revision=88

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-rcc32 -DRNV_VERSION="\"1.0.6\"" -DEXPAT_H="<expat.h>" -DUNISTD_H="<io.h>" -I.;c:\expat\source\lib;c:\borland\bcc55\include -L.;c:\borland\bcc55\lib -ernv.exe *.c libexpats_mtd.lib 
+bcc32 -DRNV_VERSION="\"1.0.7\"" -DEXPAT_H="<expat.h>" -DUNISTD_H="<io.h>" -I.;c:\expat\source\lib;c:\borland\bcc55\include -L.;c:\borland\bcc55\lib -ernv.exe *.c libexpats_mtd.lib 
--- rnv.c
+++ rnv.c
@@ -82,15 +82,15 @@ static void qname(char *name) {
 static char *msg=NULL;
 static int len_msg=-1;
 
-static void not_allowed(char *what,char *suri,char *sname) {
+static void not_allowed(char *what,char *suri,char *sname,char *val) {
   int len=strlen(what)+strlen(suri)+strlen(sname);
   if(len>len_msg) {len_msg=len; free(msg); msg=(char*)calloc(len_msg,sizeof(char));}
-  msg[sprintf(msg,what,suri,sname)]='\0';
+  msg[sprintf(msg,what,suri,sname,val)]='\0';
   error(msg);
 }
 
 #define ELEMENT_NOT_ALLOWED "element '%s^%s' not allowed"
-#define ATTRIBUTE_NOT_ALLOWED "attribute '%s' not allowed"
+#define ATTRIBUTE_NOT_ALLOWED "attribute '%s^%s=\"%s\"' not allowed"
 #define ELEMENTS_MISSING "required elements missing"
 #define ATTRIBUTES_MISSING "required attributes missing"
 #define TEXT_NOT_ALLOWED "text not allowed"
@@ -114,7 +114,7 @@ static void start_element(void *userData,const char *name,const char **attrs) {
     qname((char*)name);
     current=drv_start_tag_open(previous=current,suri,sname);
     if(current==rn_notAllowed) {
-      not_allowed(ELEMENT_NOT_ALLOWED,suri,sname);
+      not_allowed(ELEMENT_NOT_ALLOWED,suri,sname,0);
       current=drv_start_tag_open_recover(previous,suri,sname);
     }
     while(current!=rn_notAllowed) {
@@ -123,7 +123,7 @@ static void start_element(void *userData,const char *name,const char **attrs) {
       ++attrs;
       current=drv_attribute(previous=current,suri,sname,(char*)*attrs);
       if(current==rn_notAllowed) {
-	not_allowed(ATTRIBUTE_NOT_ALLOWED,suri,sname);
+	not_allowed(ATTRIBUTE_NOT_ALLOWED,suri,sname,(char*)*attrs);
 	current=drv_attribute_recover(previous,suri,sname,(char*)*attrs);
       }
       ++attrs;
