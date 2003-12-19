67d35623fc264bf397d2b5e0868dda4fac45a818
David Tolpin <dvd@davidashen.net>
2003-12-19 16:52:53 +0000

    +pr
    svn path=/trunk/trunk/; revision=120

---
+++ pr.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc,char **argv) {
+  int *primes;
+  int N,i,step;
+  primes=calloc(N=atoi(*(++argv)),sizeof(int));
+  for(i=0;i!=N;++i) primes[i]=1;
+  for(step=2;step!=256;++step) {
+    i=step+step;
+    for(;;) {
+      if(i>=N) break;
+      primes[i]=0;
+      i+=step;
+    }
+  }
+  for(i=0;i!=N;++i) if(primes[i]) printf("%x\n",i);
+}
