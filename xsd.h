7c264d01d07d3c3dbc38ad14f14daba93e55fd5f
David Tolpin <dvd@davidashen.net>
2003-12-25 11:33:07 +0000

    *** empty log message ***
    svn path=/trunk/trunk/; revision=160

--- xsd.h
+++ xsd.h
@@ -18,6 +18,4 @@ extern void xsd_init(void);
 extern int xsd_allows(char *typ,char *ps,char *s,int n);
 extern int xsd_equal(char *typ,char *val,char *s,int n);
 
-extern void (*xsd_error_handler)(char *msg);
-
 #endif
