e8d2dd72c7d9ce113fdde07df4696b8ff3d372d8
David Tolpin <dvd@davidashen.net>
2004-01-21 21:44:16 +0000

    *** empty log message ***
    svn path=/trunk/trunk/; revision=333

--- scm/rx.scm
+++ scm/rx.scm
@@ -4,5 +4,59 @@
 (load "u.scm")
 (load "rx-ranges.scm")
 
-(define rx-patterns '(none empty any choice group except range class char))
-    
+; patterns: none empty any choice group except range class char
+
+(define (rx-compile regex)
+  (letrec (
+      (s (utf8->lazy-list regex))
+      (newpat
+        (let ((cache '()))
+          (lambda (pattern)
+            (car
+              (or (member pattern cache)
+                (begin (set! cache (cons pattern cache)) cache))))))
+      (errors #f)
+      (error!
+        (lambda () 
+	  (or errors 
+	    (begin 
+	      (display (string-append "bad regex '" regex "'\n"))
+	      (set! errors #t)))))
+      (chclass
+        #f)
+      (esc
+        (lambda ()
+	  (if (null? s) (begin (error!) '(end))
+	    (let ((c (car s)))
+	      (set! s (force (cdr s))
+	      (if (>= c 128) (begin (error!) (cons 'esc c))
+	        (case (integer->char c)
+		  ((#\p) (cons 'cls (chclass)))
+		  ((#\P) (cons 'ncl (chclass)))
+		  ((#\s) '(cls . S))
+		  ((#\S) '(ncl . S))
+		  ((#\i) '(cls . I))
+		  ((#\I) '(ncl . I))
+		  ((#\c) '(cls . C))
+		  ((#\C) '(ncl . C))
+		  ((#\d) '(cls . UNd))
+		  ((#\D) '(ncl . UNd))
+		  ((#\w) '(cls . W))
+		  ((#\W) '(ncl . W))
+		  ((#\n) '(esc . 10))
+		  ((#\r) '(esc . 13))
+		  ((#\t) '(esc . 9))
+                  ((#\\\ #\| #\. #\- #\^ #\? #\* #\+ 
+		      #\{ #\} #\[ #\] #\( #\))
+		    (cons 'esc c))
+		  (else (error!) (cons 'esc c)))))))))
+      (sym
+        (lambda ()
+          (if (null? s) '(end)
+            (let ((c (car s)))
+              (set! s (force (cdr s)))
+                (or (and (= c 92) (esc)) (and (= c 46) '(ncl . NL)) 
+		  (cons 'chr c))))))
+      )
+    #f))
+      
--- scm/u.scm
+++ scm/u.scm
@@ -1,28 +1,28 @@
 ; $Id$
 ; unicode
 
-(define (utf8->list s) ; no checks, everything we get is unicode
+(define (utf8->lazy-list s) ; no checks, everything we get is unicode
   (letrec (
       (ux
-        (lambda (c ul sl n) 
-	  (if (= n 0) (left (cons c ul) sl)
+        (lambda (c sl n) 
+	  (if (= n 0) (cons c (delay (left sl)))
 	    (ux 
 	      (+ (* c 64) (remainder (char->integer (car sl)) 64)) 
-	      ul (cdr sl) (- n 1)))))
+	      (cdr sl) (- n 1)))))
       (left
-        (lambda (ul sl)
-          (if (null? sl) (reverse ul)
+        (lambda (sl)
+          (if (null? sl) '()
             (let ((c (char->integer (car sl))) (sl (cdr sl)))
               (cond 
-                  ((< c #x80) (ux c ul sl 0))
-                  ((< c #xC0) #f)
-                  ((< c #xE0) (ux (remainder c #x20) ul sl 1))
-                  ((< c #xF0) (ux (remainder c #x10) ul sl 2))
-                  ((< c #xF8) (ux (remainder c #x08) ul sl 3))
-                  ((< c #xFC) (ux (remainder c #x04) ul sl 4))
-                  ((< c #xFE) (ux (remainder c #x02) ul sl 5))))))))
-    (left '() (string->list s))))      
+                  ((< c #x80) (ux c sl 0))
+                  ((< c #xE0) (ux (remainder c #x20) sl 1))
+                  ((< c #xF0) (ux (remainder c #x10) sl 2))
+                  ((< c #xF8) (ux (remainder c #x08) sl 3))
+                  ((< c #xFC) (ux (remainder c #x04) sl 4))
+                  ((< c #xFE) (ux (remainder c #x02) sl 5))))))))
+    (left (string->list s))))      
 
+; binary search on list of ranges
 (define (u-in-ranges u ranges)
   (let between ((n 0) (m (- (vector-length ranges) 1)))
     (if (> n m) #f
