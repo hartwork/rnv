e1776cde8f4ccfbddb17189867ef7dc1d9c65eec
David Tolpin <dvd@davidashen.net>
2004-01-21 14:24:14 +0000

    +u
    svn path=/trunk/trunk/; revision=331

--- scm/u.scm
+++ scm/u.scm
@@ -1,26 +1,6 @@
 ; $Id$
 ; unicode
 
-#define ux(u,c) (((u)<<6)|(c&0x3F))
-#define u1(t) t[0]
-#define u2(t) ux(t[0]&0x1F,t[1])
-#define u3(t) ux(ux(t[0]&0xF,t[1]),t[2])
-#define u4(t) ux(ux(ux(t[0]&0x7,t[1]),t[2]),t[3])
-#define u5(t) ux(ux(ux(ux(t[0]&0x3,t[1]),t[2]),t[3]),t[4])
-#define u6(t) ux(ux(ux(ux(ux(t[0]&0x1,t[1]),t[2]),t[3]),t[4]),t[5])
-
-int u_get(int *up,char *s) {
-  unsigned char *t=(unsigned char*)s;
-  if(*t<0x80) {*up=u1(t); return 1;}
-  if(*t<0xC0) return 0;
-  if(*t<0xE0) {*up=u2(t); return (*up&B1)?2:0;}
-  if(*t<0xF0) {*up=u3(t); return (*up&B2)?3:0;}
-  if(*t<0xF8) {*up=u4(t); return (*up&B3)?4:0;}
-  if(*t<0xFC) {*up=u5(t); return (*up&B4)?5:0;}
-  if(*t<0xFE) {*up=u6(t); return (*up&B5)?6:0;}
-  return 0;
-}
-
 (define (string->u-list s)
   (letrec (
       (ux
