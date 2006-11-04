cc96193cc4ebaac727ce970d05792b7a5d20d9e0
David Tolpin <dvd@davidashen.net>
2006-11-04 22:37:45 +0000

    python module
    svn path=/trunk/trunk/; revision=466

--- py/rnvmodule.c
+++ py/rnvmodule.c
@@ -2,6 +2,7 @@
 
 #include <Python.h>
 #include <er.h>
+#include <rn.h>
 #include <rnl.h>
 #include <rnv.h>
 
@@ -9,7 +10,12 @@
 #define ERBUFLEN 1024
 static char erbuf[ERBUFLEN];
 
-int ervprintf(char *format, va_list ap) {return vsnprintf(erbuf, ERBUFLEN, format, ap);}
+int
+ervprintf(char *format, va_list ap)
+{
+  int len = strlen(erbuf);
+  return vsnprintf(erbuf+len, ERBUFLEN-len, format, ap);
+}
 
 static PyObject *
 error(PyObject *self, PyObject *args)
@@ -27,7 +33,7 @@ load(PyObject *self, PyObject *args)
 
   *erbuf = 0;
   if(!PyArg_ParseTuple(args, "s", &name)) return NULL;
-  start = rnl_fn(name);
+  if(!(start = rnl_fn(name))) start = rn_notAllowed;
   
   return Py_BuildValue("i", start);
 }
@@ -36,11 +42,11 @@ static PyObject *
 text(PyObject *self, PyObject *args)
 {
   char *text; int len, mixed;
-  int prevp = 0, curp;
+  int prevp = rn_notAllowed, curp;
 
   *erbuf = 0;
   if(!PyArg_ParseTuple(args, "is#i", &curp, &text, &len, &mixed)) return NULL;
-  rnv_text(&curp, &prevp, text, len, mixed);
+  if(!rnv_text(&curp, &prevp, text, len, mixed)) curp = rn_notAllowed;
 
   return Py_BuildValue("i", curp);
 }
@@ -49,10 +55,11 @@ static PyObject *
 start_tag_open(PyObject *self, PyObject *args)
 {
   char *name;
-  int prevp = 0, curp;
+  int prevp = rn_notAllowed, curp;
 
+  *erbuf = 0;
   if(!PyArg_ParseTuple(args,"is",&curp,&name)) return NULL;
-  rnv_start_tag_open(&curp, &prevp, name);
+  if(!rnv_start_tag_open(&curp, &prevp, name)) curp = rn_notAllowed;
 
   return Py_BuildValue("i", curp);
 }
@@ -61,10 +68,11 @@ static PyObject *
 attribute(PyObject *self, PyObject *args)
 {
   char *name, *val;
-  int prevp = 0, curp;
+  int prevp = rn_notAllowed, curp;
 
+  *erbuf = 0;
   if(!PyArg_ParseTuple(args,"iss",&curp,&name,&val)) return NULL;
-  rnv_attribute(&curp,&prevp,name,val);
+  if(!rnv_attribute(&curp,&prevp,name,val)) curp = rn_notAllowed;
 
   return Py_BuildValue("i", curp);
 }
@@ -73,10 +81,11 @@ static PyObject *
 start_tag_close(PyObject *self, PyObject *args)
 {
   char *name;
-  int prevp = 0, curp;
+  int prevp = rn_notAllowed, curp;
 
+  *erbuf = 0;
   if(!PyArg_ParseTuple(args,"is",&curp,&name)) return NULL;
-  rnv_start_tag_open(&curp, &prevp, name);
+  if(!rnv_start_tag_close(&curp, &prevp, name)) curp = rn_notAllowed;
 
   return Py_BuildValue("i", curp);
 }
@@ -85,10 +94,11 @@ static PyObject *
 end_tag(PyObject *self, PyObject *args)
 {
   char *name;
-  int prevp = 0, curp;
+  int prevp = rn_notAllowed, curp;
 
+  *erbuf = 0;
   if(!PyArg_ParseTuple(args,"is",&curp,&name)) return NULL;
-  rnv_start_tag_open(&curp, &prevp, name);
+  if(!rnv_end_tag(&curp, &prevp, name)) curp = rn_notAllowed;
 
   return Py_BuildValue("i", curp);
 }
@@ -96,19 +106,22 @@ end_tag(PyObject *self, PyObject *args)
 static PyMethodDef RnvMethods[] = {
   {"error", error, METH_VARARGS, "Return last error message."},
   {"load", load, METH_VARARGS, "Load Relax NG grammar."},
-  {"text", load, METH_VARARGS, "Validate a text element."},
-  {"sart_tag_open", start_tag_open, METH_VARARGS, "Validate the beginning of a start tag."},
+  {"text", text, METH_VARARGS, "Validate a text element."},
+  {"start_tag_open", start_tag_open, METH_VARARGS, "Validate the beginning of a start tag."},
   {"attribute", attribute, METH_VARARGS, "Validate an attribute."},
   {"start_tag_close", start_tag_close, METH_VARARGS, "Validate the end of a start tag."},
   {"end_tag", end_tag, METH_VARARGS, "Validate an end tag."},
-  {NULL, NULL, 0, NULL}        /* Sentinel */
+  {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC
 initrnv(void)
 {
+  PyObject *m;
+
   er_vprintf = &ervprintf;
   rnl_init();
   rnv_init();
-  (void)Py_InitModule("rnv", RnvMethods);
+  m = Py_InitModule("rnv", RnvMethods);
+  PyModule_AddObject(m,"notAllowed",Py_BuildValue("i",rn_notAllowed));
 }
