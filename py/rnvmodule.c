fe720953c1e2c75eb93bc16f0bf75b8d2383ef95
David Tolpin <dvd@davidashen.net>
2006-11-04 19:35:15 +0000

    rnv python integration in progress
    svn path=/trunk/trunk/; revision=463

---
+++ py/rnvmodule.c
@@ -0,0 +1,39 @@
+/* $Id */
+
+#include <Python.h>
+
+static PyObject *
+rnv_init(PyObject *self, PyObject *args)
+{
+}
+
+static PyObject *
+rnv_load(PyObject *self, PyObject *args)
+{
+}
+
+static PyObject *
+rnv_text(PyObject *self, PyObject *args)
+{
+}
+
+static PyObject *
+rnv_start_tag_open(PyObject *self, PyObject *args)
+{
+}
+
+static PyObject *
+rnv_attribute(PyObject *self, PyObject *args)
+{
+}
+
+static PyObject *
+rnv_start_tag_close(PyObject *self, PyObject *args)
+{
+}
+
+static PyObject *
+rnv_end_tag(PyObject *self, PyObject *args)
+{
+}
+
